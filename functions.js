3dd138a7c33ef592db686e52d8e1074f36eeef45
Jeff Lyons <jeffreylyons@outlook.com>
2022-01-24 18:32:47 -0500

    fix commit

--- functions.js
+++ functions.js
@@ -1,5 +1,5 @@
 function returnTwo() {
-    console.log('2')
+   
     return 2;
     
 };
@@ -7,14 +7,14 @@ function returnTwo() {
 //returnTwo();
 
 function greeting(name) {
-    console.log(`Hello, ${name}.`)
+ 
     return `Hello, ${name}.`
 };
 
 //greeting("Jeff");
 
 function add(num1, num2) {
-    console.log(num1 + num2)
+ 
     return num1 + num2
 }
 
